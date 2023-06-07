666f4cabf1f658d5ec794c88952ef39829663449
Liam <liam@noreply.invalid>
2023-06-07 21:24:19 +0930

    Or tHiz

--- src/test/java/com/example/ExamplePluginTest.java
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -1,16 +1,8 @@
 package com.example;
 
-import com.example.AutoTele.AutoTele;
-import com.example.E3t4g.e3t4g;
 import com.example.EthanApiPlugin.EthanApiPlugin;
-import com.example.LavaRunecrafter.LavaRunecrafterPlugin;
-import com.example.NightmareHelper.NightmareHelperPlugin;
 import com.example.PacketUtils.PacketUtilsPlugin;
-import com.example.PrayerFlicker.EthanPrayerFlickerPlugin;
 import com.example.UpkeepPlugin.UpkeepPlugin;
-import com.example.gauntletFlicker.gauntletFlicker;
-import com.example.harpoon2ticker.SwordFish2Tick;
-import com.example.superglass.SuperGlassMakerPlugin;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
