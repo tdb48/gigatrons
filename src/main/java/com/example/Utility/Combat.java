19dc00e9b891b9060e0dd4a0583c9a462a266186
Tom <75074400+tdb48@users.noreply.github.com>
2023-06-27 13:09:38 +0200

    combat class attack style changes

--- src/main/java/com/example/Utility/Combat.java
+++ src/main/java/com/example/Utility/Combat.java
@@ -3,8 +3,10 @@ package com.example.Utility;
 import com.example.PacketUtils.WidgetInfoExtended;
 import com.example.Packets.MousePackets;
 import com.example.Packets.WidgetPackets;
+import java.util.Arrays;
 import net.runelite.api.Skill;
 import net.runelite.api.VarPlayer;
+import net.runelite.api.widgets.WidgetInfo;
 
 public class Combat
 {
@@ -46,5 +48,41 @@ public class Combat
 		}
 		MousePackets.queueClickPacket();
 		WidgetPackets.queueWidgetActionPacket(1, SPEC_ORB_ID, -1, -1);
+	}
+
+	public static AttackStyle getAttackStyle() {
+		return Combat.AttackStyle.fromIndex(Static.getClient().getVarpValue(43));
+	}
+
+	public static enum AttackStyle {
+		FIRST(0, WidgetInfo.COMBAT_STYLE_ONE),
+		SECOND(1, WidgetInfo.COMBAT_STYLE_TWO),
+		THIRD(2, WidgetInfo.COMBAT_STYLE_THREE),
+		FOURTH(3, WidgetInfo.COMBAT_STYLE_FOUR),
+		SPELLS(4, WidgetInfo.COMBAT_SPELL_BOX),
+		SPELLS_DEFENSIVE(4, WidgetInfo.COMBAT_DEFENSIVE_SPELL_BOX),
+		UNKNOWN(-1, (WidgetInfo)null);
+
+		private final int index;
+		private final WidgetInfo widgetInfo;
+
+		private AttackStyle(int index, WidgetInfo widgetInfo) {
+			this.index = index;
+			this.widgetInfo = widgetInfo;
+		}
+
+		public int getIndex() {
+			return this.index;
+		}
+
+		public WidgetInfo getWidgetInfo() {
+			return this.widgetInfo;
+		}
+
+		public static AttackStyle fromIndex(int index) {
+			return (AttackStyle) Arrays.stream(values()).filter((x) -> {
+				return x.index == index;
+			}).findFirst().orElse(UNKNOWN);
+		}
 	}
 }
