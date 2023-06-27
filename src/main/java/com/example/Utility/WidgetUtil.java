636824a6d67acee0357ea363df397ede56dea6fc
Tom <75074400+tdb48@users.noreply.github.com>
2023-06-27 13:06:55 +0200

    widget util changes

---
+++ src/main/java/com/example/Utility/WidgetUtil.java
@@ -0,0 +1,28 @@
+package com.example.Utility;
+
+import net.runelite.api.widgets.Widget;
+
+public class WidgetUtil
+{
+
+	public static boolean isVisible(Widget widget)
+	{
+		return widget != null && !widget.isHidden() && !widget.isSelfHidden();
+	}
+
+	public static boolean hasAction(Widget widget, String action)
+	{
+		if (widget.getActions() == null)
+		{
+			return false;
+		}
+		for (String widgetAction : widget.getActions())
+		{
+			if (widgetAction.equalsIgnoreCase(action.toLowerCase()))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+}
