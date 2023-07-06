a51abf5e2454d5c965dc62e62b7cd9a8ec1cf83e
Tom <75074400+tdb48@users.noreply.github.com>
2023-07-06 23:17:14 +0200

    reattack class

---
+++ src/main/java/com/example/nexatron/tasks/general/Reattack.java
@@ -0,0 +1,30 @@
+package com.example.nexatron.tasks.general;
+
+
+import com.example.Utility.Prayers;
+import com.example.nexatron.manager.NexManager;
+import com.example.nexatron.model.constants.Stage;
+import com.example.nexatron.taskformat.StagedTask;
+import com.example.nexatron.taskformat.Task;
+import com.example.nexatron.taskformat.TaskDescriptor;
+import javax.inject.Inject;
+
+@TaskDescriptor(
+	name = "Reattack",
+	priority = -Integer.MAX_VALUE
+)
+public class Reattack extends Task
+{
+	@Inject
+	NexManager nexManager;
+
+	public boolean run()
+	{
+		if (nexManager.shouldReattack)
+		{
+			nexManager.reattackInteracting();
+		}
+		nexManager.shouldReattack = false;
+		return false;
+	}
+}
