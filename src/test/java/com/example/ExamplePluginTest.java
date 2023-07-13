8984f226d6af490fac67cae1370c26b34317832a
Tom <75074400+tdb48@users.noreply.github.com>
2023-07-13 11:12:16 +0200

    added chinbreakhandler/profiles to plugintest main

--- src/test/java/com/example/ExamplePluginTest.java
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -1,16 +1,27 @@
 package com.example;
 
+import com.example.ChinBreakHandler.ChinBreakHandlerPlugin;
 import com.example.EthanApiPlugin.EthanApiPlugin;
 import com.example.PacketUtils.PacketUtilsPlugin;
 import com.example.PathingTesting.PathingTesting;
+import com.example.betterprofiles.BetterProfilesPlugin;
 import com.example.nexatron.NexatronPlugin;
 import com.example.toagigatron.ToaGigatronPlugin;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest {
-    public static void main(String[] args) throws Exception {
-        ExternalPluginManager.loadBuiltin(EthanApiPlugin.class, NexatronPlugin.class, PacketUtilsPlugin.class,ToaGigatronPlugin.class, PathingTesting.class);
-        RuneLite.main(args);
-    }
+public class ExamplePluginTest
+{
+	public static void main(String[] args) throws Exception
+	{
+		ExternalPluginManager.loadBuiltin(
+			EthanApiPlugin.class,
+			NexatronPlugin.class,
+			PacketUtilsPlugin.class,
+			ToaGigatronPlugin.class,
+			ChinBreakHandlerPlugin.class,
+			BetterProfilesPlugin.class,
+			PathingTesting.class);
+		RuneLite.main(args);
+	}
 }
