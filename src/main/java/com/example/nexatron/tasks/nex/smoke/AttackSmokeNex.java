f8c34a0fbe77c8aca31ed263f0f0f91d70d1f0a2
Tom <75074400+tdb48@users.noreply.github.com>
2023-08-08 13:33:24 +0200

    smoke nex attack task

---
+++ src/main/java/com/example/nexatron/tasks/nex/smoke/AttackSmokeNex.java
@@ -0,0 +1,86 @@
+package com.example.nexatron.tasks.nex.smoke;
+
+
+import com.example.EthanApiPlugin.Collections.Equipment;
+import com.example.Packets.MousePackets;
+import com.example.Packets.NPCPackets;
+import com.example.Utility.Combat;
+import com.example.Utility.Movement;
+import com.example.nexatron.manager.NexManager;
+import com.example.nexatron.model.constants.Stage;
+import com.example.nexatron.taskformat.StagedTask;
+import com.example.nexatron.taskformat.TaskDescriptor;
+import java.util.ArrayList;
+import javax.inject.Inject;
+import net.runelite.api.coords.WorldPoint;
+
+@TaskDescriptor(
+	name = "Attack Smoke Nex",
+	priority = 1
+)
+public class AttackSmokeNex extends StagedTask
+{
+	@Inject
+	public AttackSmokeNex(NexManager nexManager)
+	{
+		super(nexManager, Stage.NEX_SMOKE);
+	}
+
+	public boolean execute()
+	{
+		if (nexManager.nex.centerPoint == null
+			|| nexManager.nex.nex == null)
+		{
+			return false;
+		}
+
+		// Equipping range gear if boss is healthy and we are over 75 spec, otherwise we fang it
+		ArrayList<Integer> setup = nexManager.getBossHp() >= 3200 && Combat.getSpecEnergy() >= 75 ?
+			nexManager.nex.setup.rangeNex() :
+			nexManager.nex.setup.meleeNex();
+
+		if (!nexManager.hasGearEquipped(setup))
+		{
+			nexManager.print("Equipping gear");
+			nexManager.swap(setup);
+		}
+
+		// Step under on tick 1 if boss is interacting with us
+		if (nexManager.nex.nexAttackTick == 2
+			&& nexManager.nex.nex.isInteracting()
+			&& nexManager.nex.nex.getInteracting().equals(client.getLocalPlayer()))
+		{
+			WorldPoint stepUnderTile = nexManager.socket.isMaster ?
+				nexManager.nex.masterStepUnderTile :
+				nexManager.nex.slaveStepUnderTile;
+
+			if (stepUnderTile != null)
+			{
+				nexManager.print("Stepping under");
+				Movement.move(stepUnderTile);
+				return true;
+			}
+		}
+
+		// If we have a crossbow equipped, we have 75 spec and spec is not enabled, enable spec
+		if (Equipment.search().nameContains("crossbow").first().orElse(null) != null
+			&& !Combat.isSpecEnabled()
+			&& Combat.getSpecEnergy() >= 75)
+		{
+			nexManager.print("Enabling spec");
+			Combat.toggleSpec();
+		}
+
+		// If we are not interacting, attack nex
+		if (!client.getLocalPlayer().isInteracting()
+			&& nexManager.nex.invincibleTick == 0)
+		{
+			nexManager.print("Attacking Nex");
+			MousePackets.queueClickPacket();
+			NPCPackets.queueNPCAction(nexManager.nex.nex, "Attack");
+			return true;
+		}
+
+		return false;
+	}
+}
