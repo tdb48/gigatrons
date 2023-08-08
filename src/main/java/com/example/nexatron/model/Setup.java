ef2a671584c0e7d09d701d1fcb356b2f97fc5ba1
Tom <75074400+tdb48@users.noreply.github.com>
2023-08-08 13:32:03 +0200

    Change setup to work around immutable collections

--- src/main/java/com/example/nexatron/model/Setup.java
+++ src/main/java/com/example/nexatron/model/Setup.java
@@ -3,7 +3,7 @@ package com.example.nexatron.model;
 import com.example.nexatron.manager.NexManager;
 import com.example.nexatron.model.constants.NexConst;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import javax.inject.Inject;
 import net.runelite.api.ItemID;
 
@@ -23,44 +23,47 @@ public class Setup
 
 	public ArrayList<Integer> rangeNex()
 	{
-		return (ArrayList<Integer>) List.of(
-			ItemID.ZARYTE_CROSSBOW,
-			ItemID.ARMADYL_CHESTPLATE,
-			ItemID.ARMADYL_CHAINSKIRT,
-			ItemID.AVAS_ASSEMBLER,
-			ItemID.NEITIZNOT_FACEGUARD,
-			ItemID.NECKLACE_OF_ANGUISH,
-			ItemID.TWISTED_BUCKLER,
-			ItemID.LIGHTBEARER,
-			ItemID.BARROWS_GLOVES,
-			getBolts());
+		return new ArrayList<>(
+			Arrays.asList(
+				ItemID.ZARYTE_CROSSBOW,
+				ItemID.MASORI_BODY_F,
+				ItemID.MASORI_CHAPS_F,
+				ItemID.AVAS_ASSEMBLER,
+				ItemID.NEITIZNOT_FACEGUARD,
+				ItemID.NECKLACE_OF_ANGUISH,
+				ItemID.TWISTED_BUCKLER,
+				ItemID.LIGHTBEARER,
+				ItemID.BARROWS_GLOVES,
+				getBolts()));
 	}
 
 	public ArrayList<Integer> meleeNex()
 	{
-		return (ArrayList<Integer>) List.of(
-			ItemID.OSMUMTENS_FANG,
-			ItemID.BANDOS_CHESTPLATE,
-			ItemID.ARMADYL_CHAINSKIRT,
-			ItemID.INFERNAL_CAPE,
-			ItemID.NEITIZNOT_FACEGUARD,
-			ItemID.AMULET_OF_BLOOD_FURY,
-			ItemID.DRAGON_DEFENDER,
-			ItemID.LIGHTBEARER,
-			ItemID.BARROWS_GLOVES,
-			ItemID.PRIMORDIAL_BOOTS);
+		return new ArrayList<>(
+			Arrays.asList(
+				ItemID.OSMUMTENS_FANG,
+				ItemID.BANDOS_CHESTPLATE,
+				ItemID.MASORI_CHAPS_F,
+				ItemID.INFERNAL_CAPE,
+				ItemID.NEITIZNOT_FACEGUARD,
+				ItemID.AMULET_OF_BLOOD_FURY,
+				ItemID.DRAGON_DEFENDER,
+				ItemID.LIGHTBEARER,
+				ItemID.BARROWS_GLOVES,
+				ItemID.PRIMORDIAL_BOOTS));
 	}
 
 	public ArrayList<Integer> rangeKC()
 	{
-		return (ArrayList<Integer>) List.of(
-			ItemID.TOXIC_BLOWPIPE,
-			ItemID.ARMADYL_CHESTPLATE,
-			ItemID.ARMADYL_CHAINSKIRT,
-			ItemID.AVAS_ASSEMBLER,
-			ItemID.NEITIZNOT_FACEGUARD,
-			ItemID.NECKLACE_OF_ANGUISH,
-			ItemID.LIGHTBEARER,
-			ItemID.BARROWS_GLOVES);
+		return new ArrayList<>(
+			Arrays.asList(
+				ItemID.TOXIC_BLOWPIPE,
+				ItemID.ARMADYL_CHESTPLATE,
+				ItemID.ARMADYL_CHAINSKIRT,
+				ItemID.AVAS_ASSEMBLER,
+				ItemID.NEITIZNOT_FACEGUARD,
+				ItemID.NECKLACE_OF_ANGUISH,
+				ItemID.LIGHTBEARER,
+				ItemID.BARROWS_GLOVES));
 	}
 }
