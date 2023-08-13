e5cc669b3b3c8fa05bb0fe33bca9a71cb12a81c0
Tom <75074400+tdb48@users.noreply.github.com>
2023-08-13 14:04:17 +0200

    smoke minion task

---
+++ src/main/java/com/example/nexatron/tasks/nex/smoke/AttackSmokeMinion.java
@@ -0,0 +1,92 @@
+package com.example.nexatron.tasks.nex.smoke;
+
+
+import com.example.Packets.MousePackets;
+import com.example.Packets.NPCPackets;
+import com.example.Utility.Movement;
+import com.example.nexatron.manager.GameTickManager;
+import com.example.nexatron.manager.NexManager;
+import com.example.nexatron.model.constants.Stage;
+import com.example.nexatron.taskformat.StagedTask;
+import com.example.nexatron.taskformat.TaskDescriptor;
+import java.util.ArrayList;
+import javax.inject.Inject;
+import net.runelite.api.coords.WorldPoint;
+
+@TaskDescriptor(
+	name = "Attack Smoke Minion",
+	priority = 1
+)
+public class AttackSmokeMinion extends StagedTask
+{
+	@Inject
+	public AttackSmokeMinion(NexManager nexManager)
+	{
+		super(nexManager, Stage.MINION_SMOKE);
+	}
+
+	@Inject
+	GameTickManager gameTickManager;
+
+	public boolean execute()
+	{
+		if (nexManager.nex.centerPoint == null
+			|| nexManager.nex.nex == null
+			|| nexManager.nex.fumus == null)
+		{
+			return false;
+		}
+
+		// TODO: add blowpipe or fang
+		ArrayList<Integer> setup = nexManager.nex.setup.rangeNex();
+		if (!nexManager.hasGearEquipped(setup))
+		{
+			nexManager.print("Equipping gear");
+			nexManager.swap(setup);
+		}
+
+		WorldPoint mainTile;
+		if (!nexManager.socket.isMaster
+			&& (nexManager.nex.outOfNexRange() || nexManager.nex.canStepOut()))
+		{
+			mainTile = nexManager.nex.slaveDodgeTile;
+		}
+		else
+		{
+			mainTile = nexManager.nex.getMainTile();
+		}
+		if (nexManager.nex.isNexChasingUs())
+		{
+			if (nexManager.nex.distanceToNex() <= 3)
+			{
+				nexManager.print("Stepping under nex " + nexManager.worldPointString(nexManager.nex.getUnderNex()));
+				Movement.walk(nexManager.nex.getUnderNex());
+			}
+			else if (mainTile != null
+				&& !client.getLocalPlayer().getWorldLocation().equals(mainTile))
+			{
+				nexManager.print("Moving to main tile");
+				Movement.walk(mainTile);
+			}
+			return true;
+		}
+
+		if (mainTile != null
+			&& !client.getLocalPlayer().getWorldLocation().equals(mainTile))
+		{
+			nexManager.print("Moving to main tile");
+			Movement.walk(mainTile);
+			return true;
+		}
+
+		if (!gameTickManager.isAttackWaiting()
+			&& !client.getLocalPlayer().isInteracting())
+		{
+			nexManager.print("Attacking fumus");
+			MousePackets.queueClickPacket();
+			NPCPackets.queueNPCAction(nexManager.nex.fumus, "Attack");
+			return true;
+		}
+		return false;
+	}
+}
