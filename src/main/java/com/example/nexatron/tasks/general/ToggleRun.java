5ccc182674189fed6d22e4f2fb3c12c2e649d5b1
Tom <75074400+tdb48@users.noreply.github.com>
2023-10-02 19:50:44 +0200

    added stage check to toggle run

--- src/main/java/com/example/nexatron/tasks/general/ToggleRun.java
+++ src/main/java/com/example/nexatron/tasks/general/ToggleRun.java
@@ -1,8 +1,11 @@
 package com.example.nexatron.tasks.general;
 
 import com.example.Utility.Movement;
+import com.example.nexatron.manager.NexManager;
+import com.example.nexatron.model.constants.Stage;
 import com.example.nexatron.taskformat.Task;
 import com.example.nexatron.taskformat.TaskDescriptor;
+import javax.inject.Inject;
 
 @TaskDescriptor(
 	priority = 19,
@@ -10,8 +13,16 @@ import com.example.nexatron.taskformat.TaskDescriptor;
 )
 public class ToggleRun extends Task
 {
+	@Inject
+	NexManager nexManager;
+
 	public boolean run()
 	{
+		if (nexManager.getStage().equals(Stage.MINION_SMOKE)
+			|| nexManager.getStage().equals(Stage.MINION_SHADOW))
+		{
+			return false;
+		}
 		if (!Movement.isRunEnabled() && Movement.getRunEnergy() >= 1)
 		{
 			Movement.toggleRun();
