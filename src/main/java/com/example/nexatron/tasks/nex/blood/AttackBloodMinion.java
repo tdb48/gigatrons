cb9c1d813fc3888d9734222edff187d8ef7836d6
Tom <75074400+tdb48@users.noreply.github.com>
2023-10-02 23:09:25 +0200

    blood minion task

---
+++ src/main/java/com/example/nexatron/tasks/nex/blood/AttackBloodMinion.java
@@ -0,0 +1,106 @@
+package com.example.nexatron.tasks.nex.blood;
+
+
+import com.example.EthanApiPlugin.Collections.Equipment;
+import com.example.Packets.MousePackets;
+import com.example.Packets.NPCPackets;
+import com.example.Utility.Combat;
+import com.example.Utility.Movement;
+import com.example.nexatron.manager.GameTickManager;
+import com.example.nexatron.manager.NexManager;
+import com.example.nexatron.model.constants.Stage;
+import com.example.nexatron.taskformat.StagedTask;
+import com.example.nexatron.taskformat.TaskDescriptor;
+import java.util.ArrayList;
+import javax.inject.Inject;
+import net.runelite.api.coords.WorldPoint;
+
+@TaskDescriptor(
+	name = "Attack Blood Minion",
+	priority = 1
+)
+public class AttackBloodMinion extends StagedTask
+{
+	@Inject
+	public AttackBloodMinion(NexManager nexManager)
+	{
+		super(nexManager, Stage.MINION_BLOOD);
+	}
+
+	@Inject
+	GameTickManager gameTickManager;
+
+	public boolean execute()
+	{
+		if (nexManager.nex.centerPoint == null
+			|| nexManager.nex.nex == null
+			|| nexManager.nex.cruor == null)
+		{
+			return false;
+		}
+
+		ArrayList<Integer> setup = decideSetup();
+		if (!nexManager.hasGearEquipped(setup))
+		{
+			nexManager.print("Equipping gear");
+			nexManager.swap(setup);
+		}
+
+		//Put fang on slash (signicant DPS increase)
+		if (Equipment.search().nameContains("fang").first().orElse(null) != null
+			&& !Combat.getAttackStyle().equals(Combat.AttackStyle.THIRD))
+		{
+			nexManager.print("Putting fang on slash!!");
+			Combat.toggleStyle(Combat.AttackStyle.THIRD);
+		}
+
+		// TODO: add attacking with zcb during sacrifice
+		if (nexManager.nex.sacrificeActive)
+		{
+			WorldPoint sacrificeTile = nexManager.nex.getSacrificeTile();
+			if (!nexManager.nex.sacrificeTiles.contains(nexManager.getPlayerPoint())
+				&& sacrificeTile != null
+				&& !nexManager.nex.outOfNexRange())
+			{
+				nexManager.print("Moving to " + nexManager.worldPointString(sacrificeTile));
+				Movement.walk(sacrificeTile);
+				return true;
+			}
+		}
+
+		// Attack first, then second prio is moving to our main tile
+		if (!gameTickManager.isAttackWaiting()
+			&& !client.getLocalPlayer().isInteracting())
+		{
+			nexManager.print("Attacking cruor");
+			MousePackets.queueClickPacket();
+			NPCPackets.queueNPCAction(nexManager.nex.cruor, "Attack");
+			return true;
+		}
+
+		WorldPoint standTile = nexManager.nex.getMainTile();
+		if (standTile != null
+			&& !client.getLocalPlayer().getWorldLocation().equals(standTile))
+		{
+			nexManager.print("Moving to prepath tile");
+			Movement.walk(standTile);
+			return true;
+		}
+		return false;
+	}
+
+	public ArrayList<Integer> decideSetup()
+	{
+		int distance;
+		if (nexManager.nex.sacrificeActive)
+		{
+			WorldPoint sacrificeTile = nexManager.nex.getSacrificeTile();
+			distance = nexManager.nex.wpDistanceToMinion(sacrificeTile);
+		}
+		else
+		{
+			distance = nexManager.nex.distanceToActiveMinion();
+		}
+		return distance >= 3 ? nexManager.nex.setup.rangeNex() : nexManager.nex.setup.meleeNex();
+	}
+}
