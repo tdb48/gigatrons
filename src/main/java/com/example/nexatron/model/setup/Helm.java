7a27f2ae52b50412bf71449a4d8bebc1b38821c4
Tom <75074400+tdb48@users.noreply.github.com>
2023-10-03 17:16:42 +0200

    changed helm names and removed slayer helm

--- src/main/java/com/example/nexatron/model/setup/Helm.java
+++ src/main/java/com/example/nexatron/model/setup/Helm.java
@@ -4,9 +4,8 @@ import net.runelite.api.ItemID;
 
 public enum Helm
 {
-	BUDGET_NEIT(ItemID.HELM_OF_NEITIZNOT),
-	GUCCI_NEIT(ItemID.NEITIZNOT_FACEGUARD),
-	SLAYER_HELM(ItemID.SLAYER_HELMET);
+	NEIT(ItemID.HELM_OF_NEITIZNOT),
+	FACEGUARD(ItemID.NEITIZNOT_FACEGUARD);
 	public final int itemId;
 
 	Helm(int itemId)
