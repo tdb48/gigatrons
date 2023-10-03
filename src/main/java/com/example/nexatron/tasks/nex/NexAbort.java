4954cccc07e07f34d74679c21b3b3a78d8e65e5c
Tom <75074400+tdb48@users.noreply.github.com>
2023-10-03 23:51:36 +0200

    changed abort logic so it teleports when out of brews or if the other player left

--- src/main/java/com/example/nexatron/tasks/nex/NexAbort.java
+++ src/main/java/com/example/nexatron/tasks/nex/NexAbort.java
@@ -1,13 +1,18 @@
 package com.example.nexatron.tasks.nex;
 
 
+import com.example.EthanApiPlugin.Collections.Inventory;
 import com.example.Packets.MousePackets;
 import com.example.Packets.ObjectPackets;
+import com.example.Utility.Combat;
 import com.example.nexatron.manager.NexManager;
+import com.example.nexatron.model.Consumable;
 import com.example.nexatron.model.constants.Stage;
 import com.example.nexatron.taskformat.StagedTask;
 import com.example.nexatron.taskformat.TaskDescriptor;
 import javax.inject.Inject;
+import net.runelite.api.Player;
+import net.runelite.api.widgets.Widget;
 
 @TaskDescriptor(
 	name = "Nex Abort",
@@ -29,26 +34,36 @@ public class NexAbort extends StagedTask
 			Stage.NEX_BLOOD,
 			Stage.MINION_ICE,
 			Stage.NEX_ICE,
-			Stage.NEX_START);
+			Stage.NEX_START,
+			Stage.NEX_ZAROS);
 	}
 
 	public boolean execute()
 	{
 		if (nexManager.nex.altar == null
-			|| !nexManager.nex.teleportOut)
+			|| !shouldTeleport())
 		{
 			return false;
 		}
-
+		nexManager.enableRun(true);
+		// To make sure we have an ancient item equipped when leaving!
 		if (!nexManager.hasGearEquipped(nexManager.nex.setup.rangeNex()))
 		{
 			nexManager.print("Equipping range gear");
 			nexManager.swap(nexManager.nex.setup.rangeNex());
 		}
-
 		MousePackets.queueClickPacket();
 		ObjectPackets.queueObjectAction(nexManager.nex.altar, false, "Teleport");
 		return true;
+	}
+
+	public boolean shouldTeleport()
+	{
+		Widget brew = Inventory.search().nameContains("omin brew").first().orElse(null);
+		Player otherPlayer = nexManager.socket.getOtherPlayer();
+		return nexManager.nex.teleportOut
+			|| (brew == null && Combat.getCurrentHealth() <= 60)
+			|| (otherPlayer == null && !nexManager.getStage().equals(Stage.NEX_DEAD) && !nexManager.getStage().equals(Stage.NEX_START));
 	}
 
 }
