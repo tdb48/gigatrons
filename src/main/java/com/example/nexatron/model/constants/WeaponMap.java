72978edf7833b906fc139a8773ed7a6cfcbf676a
Tom <75074400+tdb48@users.noreply.github.com>
2023-10-03 23:46:19 +0200

    updated weaponmap

--- src/main/java/com/example/nexatron/model/constants/WeaponMap.java
+++ src/main/java/com/example/nexatron/model/constants/WeaponMap.java
@@ -1,6 +1,7 @@
 package com.example.nexatron.model.constants;
 
 import java.util.HashMap;
+import net.runelite.api.ItemID;
 
 public class WeaponMap
 {
@@ -8,11 +9,18 @@ public class WeaponMap
 
 	static
 	{
-		StyleMap.put(26219, WeaponStyle.MELEE); // Fang
-		StyleMap.put(12926, WeaponStyle.RANGE); // BP
-		StyleMap.put(22323, WeaponStyle.MAGIC); // Sang
-		StyleMap.put(1215, WeaponStyle.MELEE); // Regular DDS
-		StyleMap.put(11804, WeaponStyle.MELEE); // BGS (unkitted)
+		StyleMap.put(ItemID.ZARYTE_CROSSBOW, WeaponStyle.RANGE);
+		StyleMap.put(ItemID.ARMADYL_CROSSBOW, WeaponStyle.RANGE);
+		StyleMap.put(ItemID.DRAGON_CROSSBOW, WeaponStyle.RANGE);
+		StyleMap.put(ItemID.RUNE_CROSSBOW, WeaponStyle.RANGE);
+		StyleMap.put(ItemID.DRAGON_CLAWS, WeaponStyle.MELEE);
+		StyleMap.put(ItemID.VOIDWAKER, WeaponStyle.RANGE);
+		StyleMap.put(ItemID.OSMUMTENS_FANG, WeaponStyle.MELEE);
+		StyleMap.put(ItemID.OSMUMTENS_FANG_OR, WeaponStyle.MELEE);
+		StyleMap.put(ItemID.TOXIC_BLOWPIPE, WeaponStyle.RANGE);
+		StyleMap.put(ItemID.SANGUINESTI_STAFF, WeaponStyle.MAGIC);
+		StyleMap.put(ItemID.DRAGON_DAGGER, WeaponStyle.MELEE);
+		StyleMap.put(ItemID.BANDOS_GODSWORD, WeaponStyle.MELEE);
 	}
 
 	public WeaponMap()
